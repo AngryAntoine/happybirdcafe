efdf6271cb0151af3943ed96fbf70af3de70ae92
Anton Gramenko <tony1986@yandex.ru>
2016-05-13 21:13:34 +0300

    Something to show to client-static add

--- happybirdcafe/settings.py
+++ happybirdcafe/settings.py
@@ -130,7 +130,7 @@ LOCALE_PATHS = (
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
